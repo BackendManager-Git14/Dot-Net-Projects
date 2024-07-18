c13d57a8ef8825e61e74b417768114f7d6f06389
BackendManager-Git14 <aksharpp2910@gmail.com>
2024-07-18 19:30:47 +0530

    Moved whole project to current repo

---
+++ Scrap_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Scrap_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
