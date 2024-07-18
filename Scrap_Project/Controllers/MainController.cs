8442170cd1dfa0094dbda3fd76e42c8d0f8ec479
BackendManager-Git14 <aksharpp2910@gmail.com>
2024-07-18 22:06:42 +0530

    Added model and controller

---
+++ Scrap_Project/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scrap_Project.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scrap_Project/Models/user_data.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scrap_Project.Models
+{
+    public class user_data
+    {
+        public int id { get; set; }
+
+        [Display(Name ="User Name")]
+        public string u_name { get; set; }
+
+        [Display(Name ="Email")]
+        public string email_add { get; set; }
+
+        [Display(Name ="Birth Date")]
+        public DateTime b_date { get; set; }
+
+        [Display(Name ="Contact No.")]
+        public Int64 contact { get; set; }
+        public string img_path { get; set; }
+
+        [Display(Name ="Profile Photo")]
+        public IFormFile imgfile { get; set; }
+    }
+}
