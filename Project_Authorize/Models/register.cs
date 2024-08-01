a58c08ac0c11470af956dc404f13d62da2054c89
BackendManager-Git14 <aksharpp2910@gmail.com>
2024-08-01 11:32:04 +0530

    project deployed

---
+++ Project_Authorize/Models/register.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_Authorize.Models
+{
+    public class register
+    {
+        public int Id { get; set; }
+        public string u_name { get; set; }
+        public string email { get; set; }
+        public string pswd { get; set; }
+        public string con_pswd { get; set; }
+        public Int64 contact { get; set; }
+        public string img_path { get; set; }    
+        public IFormFile profile_img { get; set; }
+
+    }
+}
--- Scrap_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Scrap_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
