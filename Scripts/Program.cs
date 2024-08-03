07d913eb150ec8b083008d9df1972a262bcda844
BackendManager-Git14 <aksharpp2910@gmail.com>
2024-08-03 10:38:15 +0530

    program.cs updated else block removed

--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -140,109 +140,15 @@ namespace scripts
                         Console.Read();
 
                     }
-
-                    else
-                    {
-                        Console.WriteLine("No birthday Today");
-                        Console.Read();
-                    }
                 }
             } //end of multiuser email logic
 
-            else //idhr current date pr single row fetch thyi hshe to aa logic use thshe
+            else
             {
-                string single_qry = "select * from user_data where MONTH(b_date) = @month AND DAY(b_date) = @day";
-                cmd = new SqlCommand(single_qry, con);
-                cmd.Parameters.Add(new SqlParameter("@month", today.Month));
-                cmd.Parameters.Add(new SqlParameter("@day", today.Day));
-
-                SqlDataReader dr = cmd.ExecuteReader();
-                dr.Read();
-
-                data single_user = new data
-                {
-                    id = Convert.ToInt16(dr["id"]),
-                    u_name = dr["u_name"].ToString(),
-                    b_date = (DateTime)dr["b_date"],
-                    email_add = dr["email_add"].ToString(),
-                    image_path = dr["image_path"].ToString()
-                };
-
-                dr.Close();
-                if (date_today == single_user.b_date.ToString("MM-dd"))
-                {
-                    string from_email_addr = "aksharpp2910@gmail.com"; 
-                    string p_num = "yntf tbup vehl hnar"; 
-
-                    // file fetching section// 
-
-                    string url = "https://raw.githubusercontent.com/BackendManager-Git14/Work_Images/main/Work_Images/Visual/";
-                    string removepath = "~/Visual/";
-                    string path = single_user.image_path.Replace(removepath, url);
-
-                    MailMessage mailMessage = new MailMessage();
-                    mailMessage.From = new MailAddress(from_email_addr);
-                    mailMessage.Subject = "idk";
-                    mailMessage.To.Add(new MailAddress(single_user.email_add));
-                    mailMessage.Body = $@"
-
-                                                <html>
-                                                <head>
-
-                                                </head>
-                                                <body>
-                                                <table width='100%' cellpadding='0' cellspacing='0' border='0'>
-                                                <tr>
-                                                <td align='center'>
-                                                    <table class='template' cellpadding='0' cellspacing='0' border='0' background=""https://raw.githubusercontent.com/BackendManager-Git14/Web-Work-1/main/visual%20resources/bday_template.png"" style=""max-width: 487px; width: 487px;background-size: contain;"">
-                                                        <tr height=""150px"">
-                                                            <td>&nbsp;</td>
-                                                        </tr>
-                                                        <tr height=""26px"">
-                                                            <td>
-                                                                <p class='name' style=""text-align: center; color: #fff;font-size: 18px;"">{single_user.u_name} </p>  
-                                                            </td>
-                                                        </tr>
-                                                        <tr height=""120px"">
-                                                            <td align=""center"">
-                                                                <table width='100%' cellpadding='0' cellspacing='0' border='0' style=""margin-top: 50px;"">
-                                                                    <tr>
-                                                                        <td align=""center"">
-                                                                            <img src='https://raw.githubusercontent.com/BackendManager-Git14/Web-Work-1/main/visual%20resources/profile.png' alt='User Image' class='user-image' style=""max-width: 110px;"">
-                                                                        </td>
-                                                                    </tr>
-                                                                </table>
-                                                            </td>
-                                                        </tr>
-                                                        <tr height=""150px"">
-                                                            <td>&nbsp;</td>
-                                                        </tr>
-                                                    </table>
-                                                </td>
-                                                </tr>
-                                                </table>
-                                                </body>
-                                                </html>
-
-
-                           ";
-                    mailMessage.IsBodyHtml = true;
-                    var smtpclient = new SmtpClient("smtp.gmail.com")
-                    {
-                        Port = 587,
-                        UseDefaultCredentials = false,
-                        Credentials = new NetworkCredential(from_email_addr, p_num),
-                        EnableSsl = true
-                    };
-                    smtpclient.Send(mailMessage);
-                    Console.Read();
-                }
-                else
-                {
-                    Console.WriteLine("No birthday Today");
-                    Console.Read();
-                }
+                Console.WriteLine("No birthday Today");
+                Console.Read();
             }
+
         }
     }
 }
